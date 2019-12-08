7acfa82691ec803d85817ac0db36361029ffdcde
Chaostheorie <chaos4747@gmail.com>
2019-12-08 21:12:56 +0100

    migrate Backend

--- Backend/interactive.py
+++
@@ -1,8 +0,0 @@
-# File for tests run with hydrogen
-
-from app.models import tournaments, User
-from datetime import date
-
-
-u = User.query.first()
-tournaments.create_tournament("Test", u, date.today())
